645623949332df5e24e4a45e6f8f3623cf81e705
Tero Keränen <tero-keranen@hotmail.com>
2024-04-13 21:14:57 +0300

    add a function that converts the timestamps of events into a more readable format and use that in landingPageEvents component

--- src/components/LandingPageEvents.jsx
+++ src/components/LandingPageEvents.jsx
@@ -1,17 +1,18 @@
+import { changeEventsTimestamp } from "../utils/eventsTimeStamp";
 
 
 const LandingPageEvents = ({events}) => {
 
-    
 
     const renderEvents = () => {
 
         
         return events.map((event, index) => {
-            console.log(event)
+            console.log(event.timestamp)
             const userEmail = event.user && event.user.email ? event.user.email : "Ei sähköpostia";
             return (
                 <div className="border my-2 p-2 rounded-lg" key={index}>
+                        {event.timestamp && <p>{changeEventsTimestamp(event.timestamp)}</p>}
                         <p>{event.type}</p>
                         <p>{userEmail}</p>
                         
