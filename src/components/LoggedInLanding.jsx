3e8bb48cceb6c71d6b5f7b2a26f68f6fe891d7ca
Tero Keränen <tero-keranen@hotmail.com>
2024-07-16 21:45:02 +0300

    update loggedinlanding styles

--- src/components/LoggedInLanding.jsx
+++ src/components/LoggedInLanding.jsx
@@ -21,7 +21,7 @@ const LoggedInLanding = ({events, worksites, userInfo}) => {
     
 
     return (
-        <div className="flex flex-col lg:flex-row justify-between" >
+        <div className="flex flex-col lg:flex-row justify-between h-screen" >
             <div className="w-full md:w-4/5 mx-auto " style={{backgroundImage: `url(${logoImage})`, backgroundSize: '50%', backgroundRepeat: 'no-repeat',backgroundPosition: "center", }}>
                 
 
